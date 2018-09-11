7a285da693e41c5c27459f2e99e342922f709105
Lukas Imrich <lukas@imrich.design>
2018-09-11 18:02:52 +0200

    Gatsby Build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Lukas Imrich Design',
+    description: 'Personal website',
+    keywords: 'design',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
---
+++ public/path----ca8c92057beebc2357b4.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Lukas Imrich Design"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----ca8c92057beebc2357b4.js.map
